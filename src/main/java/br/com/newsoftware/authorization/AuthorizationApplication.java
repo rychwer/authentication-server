271254d4ff35c90e02c1fcb10131a285b9b46f2c
Richer Santos <richer.santos@outlook.com>
2020-05-02 16:59:12 -0300

    Correção do dockerfile e da configuração do config server

--- src/main/java/br/com/newsoftware/authorization/AuthorizationApplication.java
+++ src/main/java/br/com/newsoftware/authorization/AuthorizationApplication.java
@@ -2,10 +2,11 @@ package br.com.newsoftware.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class AuthorizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
